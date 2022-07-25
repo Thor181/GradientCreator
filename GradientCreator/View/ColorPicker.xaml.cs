4f93740f930ea180d8a836dad161dc928eb32b76
TorgashinAA <torgashin.aa@yandex.ru>
2022-07-25 18:16:10 +0700

    1

--- GradientCreator/View/ColorPicker.xaml.cs
+++ GradientCreator/View/ColorPicker.xaml.cs
@@ -20,9 +20,30 @@ namespace GradientCreator.View
     /// </summary>
     public partial class ColorPicker : UserControl
     {
+        private BitmapImage bitmap;
         public ColorPicker()
         {
             InitializeComponent();
+            Uri uri = new Uri("pack://application:,,,/Resources/colormap.png");
+            bitmap = new BitmapImage(uri);
         }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
+
+            Point p = e.GetPosition((IInputElement)sender);
+            Pixel[,] pixels = new Pixel[bitmap.PixelHeight, bitmap.PixelWidth];
+            bitmap.CopyPixels(pixels, bitmap.PixelWidth * 4, 0);
+
+            var JUST_BREAKPOINT = string.Empty;
+        }
+    }
+    public struct Pixel
+    {
+        public byte Red;
+        public byte Green;
+        public byte Blue;
+        public byte Alpha;
     }
 }
